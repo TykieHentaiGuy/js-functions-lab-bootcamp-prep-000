e50fabf70bad2bf8d35ccbfdb61d102ee30ec947
Kwame Olenja <oriedo_the_wonderful@yahoo.com>
2018-02-21 08:45:57 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -6,5 +6,15 @@ happyholidays(){
 }
 
 happyHolidaysTo(){
-  console.log(Happy holidays, ${Joe})
+  console.log('Happy holidays, ${Joe}!')
+}
+
+function
+happyHolidayTo(){
+  console.log('Happy ${holiday},${Susan}!')
+}
+
+function
+holidayCountdown(){
+  return string ("It's ${20} days until ${Mother's Day}")
 }
